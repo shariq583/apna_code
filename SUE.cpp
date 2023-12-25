aa69f075397cb91c2f8badef1b90843032e0ce18
shariq583 <gazdarshariq0@gmail.com>
2023-12-25 15:18:53 +0500

    12/25/2023

---
+++ SUE.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+struct hotel
+{
+    int room_number ;
+    int floor ;
+    bool reser_;
+
+} ;
+
+
+int main ()
+{
+    // struct hotel Aj;
+    // Aj.room_number=300;
+    // Aj.floor=1;
+    // Aj.reser_=1;
+
+    // cout<<"\nThe room number of AJ is "<<Aj.room_number;
+    // cout<<"\nThe Floor number of AJ is "<<Aj.floor;
+    // cout<<"\nIs Aj reserved "<<Aj.reser_;
+
+    //the program is to make A hotel reservation system 
+    
+
+
+
+return  0;
+}
