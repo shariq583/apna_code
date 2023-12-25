43577b1c932c2bf157f28cd0a58112b207beecef
shariq583 <gazdarshariq0@gmail.com>
2023-12-25 15:38:25 +0500

    code added

---
+++ HRS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+int main ()
+{
+    srand(time(0));
+
+    int Cid=(rand())/2;
+     
+return 0;
+}
---
+++ exp_HRS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+int main ()
+{
+ srand(time(0)); 
+  
+    for (int i = 0; i < 4; i++) 
+       rand() << " "; 
+    
+return 0;
+}
