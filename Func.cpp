567798c7bdccfdf44f786937c480d5135fd7ab0f
shariq583 <gazdarshariq0@gmail.com>
2023-12-29 14:25:54 +0500

    29/12/2023

--- Func.cpp
+++ Func.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 
-
-
-
-
 using namespace std;
+
+
+void swapRef(int &a ,int &b ){
+    int temp =a ;
+    a=b;
+    b=temp ;
+   
+}
+
 int main ()
 {
+int a =3; 
+int b =4;
+    cout<<"\n The value of a is"<<a;
+    cout<<"\n The value of a is"<<b;    
+swapRef(a,b );
+    cout<<"\n The value of a is"<<a;
+    cout<<"\n The value of a is"<<b;    
+
+
 
 
 
