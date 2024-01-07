a296efdb6af4d20e2b70c6918463210341e6a096
shariq583 <gazdarshariq0@gmail.com>
2024-01-07 16:37:03 +0500

    Shop_CPP done!

--- Cpp_Code/OOP_Shop.cpp
+++ Cpp_Code/OOP_Shop.cpp
@@ -7,24 +7,41 @@ class Shop
 
 public:
     int counter;
-    void initCounter() { counter = 1; }
+    void initCounter() { counter = 0; }
     void setPrice();
     void dispPrice();
 };
 void Shop::setPrice()
 {
-
     cout << "\nEnter Id of item :";
     cin >> itemId[counter];
-    cout << "\nEnter it's price of item";
+    cout << "\nEnter it's price of item :";
     cin >> item_Pr[counter];
     counter++;
 }
-
+void Shop ::dispPrice()
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "\n"<< i + 1 << ".The price of item with Id " << itemId[i] << " is :" << item_Pr[i] << " Rs";
+    }
+}
 int main()
 {
     Shop dukaan;
-    dukaan.setPrice();
+    int products;
+    cout<<"\n======================";
+    cout<<"\nShop managing Software";
+    cout<<"\n======================";
+   cout << "\nHow many products do you have";
+    cin >> products;
+    dukaan.initCounter();
+    for (int i = 0; i < products; i++)
+    {
+        dukaan.setPrice();
+    }
+    
+    dukaan.dispPrice();
 
     return 0;
 }
